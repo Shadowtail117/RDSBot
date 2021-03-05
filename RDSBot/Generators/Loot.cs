8cee68fd5c29767289500135d6e6ff71c95d094c
Shadow <shadowtail117@gmail.com>
2021-03-05 10:48:35 -0800

    Add loot class

---
+++ RDSBot/Generators/Loot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RDSBot.Generators
+{
+    class Loot : Generator
+    {
+        public override string Name { get; set; } = "Loot Table";
+        public override string Command { get; set; } = "loot";
+
+        public override string Output(string[] args)
+        {
+            
+        }
+    }
+}
